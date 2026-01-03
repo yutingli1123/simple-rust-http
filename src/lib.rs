03cd455d5e522b743b816bf87b16feab9fc6232c
Yuting Li <yuting.li@mail.mcgill.ca>
2026-01-03 02:09:22 -0500

    feat: implement graceful shutdown for ThreadPool (main)
    Wrap the sender in an Option to facilitate closing the channel
    during shutdown. Implement the Drop trait for ThreadPool to
    ensure all worker threads are joined before the pool is destroyed.

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ use std::thread;
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
-    sender: mpsc::Sender<Job>,
+    sender: Option<mpsc::Sender<Job>>,
 }
 
 impl ThreadPool {
@@ -20,7 +20,10 @@ impl ThreadPool {
             workers.push(Worker::new(id, Arc::clone(&receiver)));
         }
 
-        ThreadPool { workers, sender }
+        ThreadPool {
+            workers,
+            sender: Some(sender),
+        }
     }
 
     pub fn execute<F>(&self, f: F)
@@ -29,7 +32,17 @@ impl ThreadPool {
     {
         let job = Box::new(f);
 
-        self.sender.send(job).unwrap();
+        self.sender.as_ref().unwrap().send(job).unwrap();
+    }
+}
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        for worker in &mut self.workers.drain(..) {
+            println!("Shutting down worker {}", worker.id);
+
+            worker.thread.join().unwrap();
+        }
     }
 }
 
